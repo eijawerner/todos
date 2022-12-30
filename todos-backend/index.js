cea3f1cbbb20d3b28743bef5409dc5f3cbb20c61
Eija Werner <github@eija.se>
2022-12-30 10:32:35 +0100

    Setup apollo server with web socket

--- todos-backend/index.js
+++ todos-backend/index.js
@@ -1,6 +1,12 @@
-const { gql, ApolloServer } = require("apollo-server");
-const { Neo4jGraphQL } = require("@neo4j/graphql");
 const neo4j = require("neo4j-driver");
+const { gql, ApolloServer } = require("apollo-server-express");
+const { Neo4jGraphQL, Neo4jGraphQLSubscriptionsSingleInstancePlugin } = require("@neo4j/graphql");
+const { createServer } = require("http");
+const { WebSocketServer } = require("ws");
+const { useServer } = require("graphql-ws/lib/use/ws");
+const express = require('express');
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+
 require("dotenv").config();
 
 const typeDefs = gql`
@@ -28,15 +34,61 @@ const driver = neo4j.driver(
     neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
 );
 
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
-neoSchema.getSchema()
-    .then((schema) => {
-        const server = new ApolloServer({
-            schema: schema,
-        });
+const neoSchema = new Neo4jGraphQL({ 
+  typeDefs, 
+  driver, 
+  plugins: {
+    subscriptions: new Neo4jGraphQLSubscriptionsSingleInstancePlugin()}
+  });
 
-        server.listen().then(({ url }) => {
-            console.log(`GraphQL server ready on ${url}`);
-        });
-    }).catch(error => console.log(error));
+async function main() {
+  const app = express();
+  const httpServer = createServer(app);
+  const wsServer = new WebSocketServer({
+      server: httpServer,
+      path: "/graphql",
+  });
+
+  const schema = await neoSchema.getSchema();
+  const serverCleanup = useServer({
+      schema
+  }, wsServer);
+
+  const server = new ApolloServer({
+    cors: true,
+    csrfPrevention: false,
+      schema,
+      plugins: [
+          ApolloServerPluginDrainHttpServer({
+              httpServer
+          }),
+          {
+              async serverWillStart() {
+                  return {
+                      async drainServer() {
+                          await serverCleanup.dispose();
+                      },
+                  };
+              },
+          },
+      ],
+  });
+  await server.start();
+
+  const corsOptions = {
+    origin: ["http://localhost:3000"]
+  };
+  server.applyMiddleware({
+      app,
+      cors: corsOptions,
+      path: "/",
+  });
+
+  const PORT = 4000;
+  httpServer.listen(PORT, () => {
+      console.log(`Server is now running on http://localhost:${PORT}`);
+  });
+}
+
+main();
 
