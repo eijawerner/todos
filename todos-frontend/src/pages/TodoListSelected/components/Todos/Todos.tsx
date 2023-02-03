8fcea92bd26b02fdb4b9c998a0881e98e8547bd9
Eija Werner <github@eija.se>
2023-02-03 10:35:36 +0100

    Changed port and started polling again
    also ignore .vscode folder
    
    also autoformatting applied on Todos.tsx

--- todos-frontend/src/pages/TodoListSelected/components/Todos/Todos.tsx
+++ todos-frontend/src/pages/TodoListSelected/components/Todos/Todos.tsx
@@ -1,15 +1,19 @@
-import styled from 'styled-components';
-import { StyledProps, Todo, TodoListsData } from '../../../../common/types/Models';
-import React, { useEffect, useState } from 'react';
-import { style } from './Todos.style';
-import { TodoRow } from './components/TodoRow/TodoRow';
-import { Button } from '../../../../common/components/Button/Button';
-import { useMutation, useQuery } from '@apollo/client';
-import { queries } from '../../Queries';
-import { useInterval } from '../../../../common/hooks/Time';
+import styled from "styled-components";
+import {
+  StyledProps,
+  Todo,
+  TodoListsData,
+} from "../../../../common/types/Models";
+import React, { useEffect, useState } from "react";
+import { style } from "./Todos.style";
+import { TodoRow } from "./components/TodoRow/TodoRow";
+import { Button } from "../../../../common/components/Button/Button";
+import { useMutation, useQuery } from "@apollo/client";
+import { queries } from "../../Queries";
+import { useInterval } from "../../../../common/hooks/Time";
 
 export type TodosProps = StyledProps & {
-    listName: string;
+  listName: string;
 };
 
 const StyledTodoList = styled.ul`
@@ -19,69 +23,85 @@ const StyledTodoList = styled.ul`
   margin: 0;
 `;
 
-function TodosBase({listName}: TodosProps) {
-    const loadTodoData = useQuery<TodoListsData>(
-        queries.GET_TODOS_IN_TODOLIST_WITH_NAME,
-        { variables: { listName: listName}}
-    )
-    const [ addTodo, addTodoData] = useMutation(queries.CREATE_TODO)
-    const [todos, setTodos] = useState<Todo[] | null>(null)
-    const [loadingData, setLoadingData] = useState<boolean>(false)
+function TodosBase({ listName }: TodosProps) {
+  const loadTodoData = useQuery<TodoListsData>(
+    queries.GET_TODOS_IN_TODOLIST_WITH_NAME,
+    { variables: { listName: listName } }
+  );
+  const [addTodo, addTodoData] = useMutation(queries.CREATE_TODO);
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [loadingData, setLoadingData] = useState<boolean>(false);
 
-    useEffect(() => {
-        const todoList: Todo[] = (loadTodoData.data && loadTodoData.data.todoLists.length > 0 )? loadTodoData.data?.todoLists[0].todos : []
-        setTodos(todoList)
-    }, [listName, loadTodoData.data])
+  useEffect(() => {
+    const todoList: Todo[] =
+      loadTodoData.data && loadTodoData.data.todoLists.length > 0
+        ? loadTodoData.data?.todoLists[0].todos
+        : [];
+    setTodos(todoList);
+  }, [listName, loadTodoData.data]);
 
-    const reloadTodosList = () => {
-        loadTodoData.refetch()
-            .then(r => console.log('reloaded todos'))
-            .catch(error => console.log(error))
+  const reloadTodosList = () => {
+    loadTodoData
+      .refetch()
+      .then((r) => console.log("reloaded todos"))
+      .catch((error) => console.log(error));
+  };
+
+  useInterval(reloadTodosList, 60 * 1000);
+
+  const handleAddTask = (listName: string) => {
+    addTodo({
+      variables: { listName: listName, task: "" },
+    })
+      .then((result) => {
+        const todoList = result.data.updateTodoLists.todoLists[0].todos;
+        setTodos(todoList);
+        reloadTodosList();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const handleDeletedTodo = (id: string) => {
+    if (todos) {
+      const updatedList = todos.filter((todo) => todo.id !== id);
+      setTodos(updatedList);
+      reloadTodosList();
     }
+  };
 
-    // useInterval(reloadTodosList, 2000)
-
-    const handleAddTask = (listName: string) => {
-        addTodo({
-            variables: { listName: listName, task: ''}
-        })
-            .then(result => {
-                const todoList = result.data.updateTodoLists.todoLists[0].todos
-                setTodos(todoList)
-                reloadTodosList()
-
-        })
-            .catch(error => console.log(error))
+  const handleEditedTodo = () => {
+    if (todos) {
+      reloadTodosList();
     }
+  };
 
-    const handleDeletedTodo = (id: string) => {
-        if (todos) {
-            const updatedList = todos.filter(todo => todo.id !== id)
-            setTodos(updatedList)
-            reloadTodosList()
-        }
-    }
+  if (loadTodoData.loading) return <p>Loading...</p>;
+  if (loadTodoData.error)
+    return <p>{`Error: ${loadTodoData.error.message}`}</p>;
 
-    const handleEditedTodo = () => {
-        if (todos) {
-            reloadTodosList()
-        }
-    }
-
-    if (loadTodoData.loading) return <p>Loading...</p>
-    if (loadTodoData.error) return <p>{`Error: ${loadTodoData.error.message}`}</p>
-
-    return (
-        <>
-            { todos && <StyledTodoList>
-                { todos.map((todo, idx) => (<TodoRow key={todo.id} todo={todo} onDeleted={handleDeletedTodo} onEdited={handleEditedTodo} />))}
-            </StyledTodoList>}
-            <Button appearance='primary' onClick={() => handleAddTask(listName)} text={'New task'} />
-        </>
-    );
-
+  return (
+    <>
+      {todos && (
+        <StyledTodoList>
+          {todos.map((todo, idx) => (
+            <TodoRow
+              key={todo.id}
+              todo={todo}
+              onDeleted={handleDeletedTodo}
+              onEdited={handleEditedTodo}
+            />
+          ))}
+        </StyledTodoList>
+      )}
+      <Button
+        appearance="primary"
+        onClick={() => handleAddTask(listName)}
+        text={"New task"}
+      />
+    </>
+  );
 }
 
 export const Todos = styled(TodosBase)`
   ${style}
-`
+`;
